4bf3718497dffdca67106aa0617d3ed4cfb12435
Kene <kayyungx@gmail.com>
2023-09-05 15:59:59 +0100

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { GraphQLDateTime } from 'graphql-iso-date';
+import { PartnerResolver } from './partners/partners.resolver';
+import { PartnersModule } from './partners/partners.module';
 
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      playground: false,
+      resolvers: { DateTime: GraphQLDateTime },
+      subscriptions: {
+        'graphql-ws': true,
+        'subscriptions-transport-ws': true,
+      },
+    }),
+    PartnersModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PartnerResolver],
 })
 export class AppModule {}
