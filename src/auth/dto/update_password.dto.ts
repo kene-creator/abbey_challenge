4c90dc4ab25e16364b6d42b640978dc1cdd589a7
Kene <kayyungx@gmail.com>
2023-09-05 16:26:24 +0100

    add auth dtos

---
+++ src/auth/dto/update_password.dto.ts
@@ -0,0 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, MinLength } from 'class-validator';
+
+export class UpdatePasswordDto {
+  @IsString()
+  @MinLength(6)
+  @ApiProperty({
+    description: 'The new password for the user.',
+    minLength: 6,
+    example: 'newPassword123',
+  })
+  password: string;
+}
